69c2501ae7c516a68a1cd6371f2c209ab91d4e3e
bear小熊帽 <>
2023-02-10 11:57:46 +0800

    修复: :bug: jsx问题修复

--- play/vite.config.ts
+++ play/vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import DefineOptions from 'unplugin-vue-define-options/vite'
-import jsx from '@vitejs/plugin-vue-jsx'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), DefineOptions(), jsx()],
+  plugins: [vue(), DefineOptions(), vueJsx()],
   build: {
     rollupOptions: {
       input: './index.html'
