87fc3ea6a3ce06d97b2e9512056bd769a3aa80c8
MJhula <842749609@qq.com>
2023-02-10 13:53:16 +0800

    feature:drawer

--- play/src/App.vue
+++ play/src/App.vue
@@ -1,18 +1,126 @@
-<script setup lang="ts">
-// console.log(Icon);
-import BIcon from '@bear-ui/components/icon'
-import { AddCircle } from '@vicons/ionicons5'
-console.log(BIcon)
+<!-- <script setup lang="ts">
+import BDrawer from '@bear-ui/components/drawer'
+
+
+</script> -->
+
+<script>
+import BDrawer from '@bear-ui/components/drawer'
+// import BModal from '@bear-ui/components/modal'
+export default {
+  components: {
+    BDrawer,
+  },
+  data() {
+    return {
+      isShowDrawer1: false,
+      isShowDrawer2: false,
+      isShowDrawer3: false,
+      isShowDrawer4: false,
+    };
+  },
+  methods: {
+    topOpen() {
+      this.isShowDrawer1 = true;
+    },
+    bottomOpen() {
+      this.isShowDrawer2 = true;
+    },
+    leftOpen() {
+      this.isShowDrawer3 = true;
+    },
+    rightOpen() {
+      this.isShowDrawer4 = true;
+    },
+    handleClose(close) {
+      const msg = confirm("确认关闭close()函数关闭")//当用户点击确认按钮时，返回true;
+      if (msg === true){
+        console.log("close")
+        close();
+      }
+      else{
+        console.log("error")
+      }
+    },
+  },
+};
 </script>
 
+
 <template>
-  <b-icon :color="'red'">
+  <!-- <b-icon :color="'red'">
     <AddCircle></AddCircle>
   </b-icon>
   <b-icon>
     <AddCircle></AddCircle>
-  </b-icon>
+  </b-icon> -->
+  <b-modal></b-modal>
   <div>测试环境</div>
+  <b-drawer
+    v-model:is-show-drawer="isShowDrawer1"
+    title="上方弹出direction='top'"
+    direction="top"
+    :before-close="handleClose"
+    :show-close-icon="false"
+    :is-show-header="true"
+    :mask="true"
+    :click-mask-close="true"
+  ></b-drawer>
+  <b-drawer
+    v-model:is-show-drawer="isShowDrawer2"
+    title="下方弹出"
+    direction="bottom"
+    :is-show-header="true"
+    :show-close-icon="true"
+  >
+    <h1>:isShowHeader="false"去掉抽屉的头部内容</h1>
+  </b-drawer>
+  <b-drawer
+    v-model:is-show-drawer="isShowDrawer3"
+    title="左方弹出"
+    direction="left"
+    :is-show-header="true"
+    :show-close-icon="true"
+  >
+    <h1>:isShowHeader="false"去掉抽屉的头部内容</h1>
+  </b-drawer>
+  <b-drawer
+    v-model:is-show-drawer="isShowDrawer4"
+    title="右方弹出"
+    direction="right"
+    :is-show-header="true"
+    :show-close-icon="true"
+  >
+    <h1>:isShowHeader="false"去掉抽屉的头部内容</h1>
+  </b-drawer>
+  <button
+    type="success"
+    plain
+    @click="topOpen"
+  >
+    上方弹出
+  </button>
+  <button
+    type="success"
+    plain
+    @click="bottomOpen"
+  >
+    下方弹出
+  </button>
+  <button
+    type="success"
+    plain
+    @click="leftOpen"
+  >
+    左侧弹出
+  </button>
+  <button
+    type="success"
+    plain
+    @click="rightOpen"
+  >
+    右侧弹出
+  </button>
 </template>
 
-<style scoped></style>
+<style></style>
--- play/src/main.ts
+++ play/src/main.ts
@@ -2,8 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import BIcon from '@bear-ui/components/icon'
+import BDrawer from '@bear-ui/components/drawer'
 import '@bear-ui/theme-chalk/src/index.scss'
-const plugins = [BIcon]
+const plugins = [BIcon,BDrawer]
 const app = createApp(App)
 plugins.forEach(plugin => app.use(plugin))
 app.mount('#app')
