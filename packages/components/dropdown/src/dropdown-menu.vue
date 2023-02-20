e0dcede91d41e5397d29f888e484ff4da9850df1
zxzjames <1043263923@qq.com>
2023-02-20 17:52:42 +0800

    特性：添加Menu、Dropdown组件

---
+++ packages/components/dropdown/src/dropdown-menu.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-show="showDropdown" :class="[bem.b()]">
+    <slot @close="closeList"></slot>
+    <b-dropdown-item @close="closeList">Action 1</b-dropdown-item>
+  </div>
+</template>
+
+<script setup>
+import { inject } from 'vue'
+import { createNamespace } from '@bear-ui/hooks/use-namespace'
+
+defineOptions({
+  name: 'b-dropdown-menu'
+})
+const bem = createNamespace('dropdown-menu')
+
+const showDropdown = inject('showDropdown')
+const closeList = () => {
+  showDropdown.value = false
+}
+console.log(showDropdown.value)
+</script>
+
+<style lang="scss" scoped></style>
