b6abebc5ac424ee0dd8aa691b77e8498cef48c9b
bear小熊帽 <>
2023-03-06 22:22:04 +0800

    测试: :test_tube: 解决无法渲染jsx问题，tree组件补充用例

--- packages/components/tree/__test__/tree.test.ts
+++ packages/components/tree/__test__/tree.test.ts
@@ -1,11 +1,28 @@
-import { render } from '@testing-library/vue'
+import { mount } from '@vue/test-utils'
 import { describe, expect, test, vi } from 'vitest'
 import { nextTick } from 'vue'
 import { BTree } from '../index'
 describe('b-tree', () => {
   test('should work with import on demand', () => {
     console.log(BTree)
-    const { getByRole } = render(BTree)
-    getByRole('tree')
+    mount(BTree)
+  })
+  test('should accept proper options', () => {
+    mount(BTree, {
+      props: {
+        data: [
+          {
+            label: '123',
+            key: '123',
+            children: [
+              {
+                label: '123',
+                key: '123'
+              }
+            ]
+          }
+        ]
+      }
+    })
   })
 })
